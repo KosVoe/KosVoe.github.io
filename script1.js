8941a5f37f5616e8ab95f305fe2e4e7b2540ab3a
Константин <39651590+KosVoe@users.noreply.github.com>
2019-01-21 23:27:50 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,20 @@
+"use strict";
+const login = "admin";
+const password = "m4ng0h4ckz";
+let message;
+const userLogin = prompt("Введите Логин");
+if (!userLogin) {
+  message = "Отменено пользователем!";
+} else if (userLogin !== login) {
+  message = "Доступ запрещен, неверный логин!";
+}
+
+const userPassword = prompt("Введите пароль");
+if (!userPassword) {
+  message = "Отменено пользователем!";
+} else if (userPassword !== password) {
+  message = "Доступ запрещен, неверный пароль!";
+} else {
+  message = "Добро пожаловать!";
+}
+alert(message);
